1ba296cb5dfbf8a25feb22c555b9735010f53d62
ayu15 <53610854+ayu15@users.noreply.github.com>
2020-09-09 21:08:49 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CountryDropdown from './CountryDropdown';
+import CurrencyDropdown from './CurrencyDropdown';
+
+const App = () => {
+
+    return(
+        <>
+            <CountryDropdown />
+            <CurrencyDropdown />
+        </>
+    );
+}
+
+export default App;
